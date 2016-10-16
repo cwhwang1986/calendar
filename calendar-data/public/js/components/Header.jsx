0618451fada45e094e6a8254faf45904983ee369
cwhwang1986 <cwhwang1986@gmail.com>
2016-10-15 17:40:48 -0700

    Add the header

---
+++ calendar-data/public/js/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='courseraLogoWrapper'>
+        <img src='../../img/coursera-logo.svg' className='courseraLogo' alt='Coursera'/>
+      </div>
+    </div>
+  );
+};
+export default Header;
